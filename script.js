6bfa58d8d6ec5095d8b342e4802540c5ecd7079c
emabeee <105249976+emabeee@users.noreply.github.com>
2022-08-10 18:08:34 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+function button() {
+  var x =
+      document.getElementById("important-try-again");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function button2() {
+  var x =
+      document.getElementById("important-correct");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function button3() {
+  var x =
+      document.getElementById("style-try-again");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function button4() {
+  var x =
+      document.getElementById("style-correct");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function button5() {
+  var x =
+      document.getElementById("proximity-correct");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function button6() {
+  var x =
+      document.getElementById("proximity-try-again");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
